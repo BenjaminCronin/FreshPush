d6719f69dcc035c99e171fffab7f5a092d30439b
Benjamin Cronin <bcronin720@gmail.com>
2017-03-01 16:25:00 -0800

    Auton Debug

--- Autons/LeftAuton.h
+++ Autons/LeftAuton.h
@@ -14,21 +14,22 @@ void leftAuton(){
 	vroomPush(-127, -127, 500);// ^
 	vroomPush(127, 127, 1700);//Shoves preload under fence
 	vroomPush(-127, -127, 1500);//Back to original position
-	vroomPush(-127, 127, 400);//Turns toward star on left of robit
+	vroomPush(-127, 127, 300);//Turns toward star on left of robit
 	vroomPush(127, 127, 600);//forward to get star
-	vroomPush(127, -127, 600);//turns back towards fence
+	vroomPush(127, -127, 250);//turns back towards fence
 	vroomPush(127, 127, 1700);//forward to shove star under fence
-	for(int p = 0; p < 4; p++){//shoves star under fence 4 times
+	for(int p = 0; p < 2; p++){//shoves star under fence 4 times
 		vroomPush(-127, -127, 500);// ^
 		vroomPush(127, 127, 500);// ^
 	}
+	vroomPush(-127, -127, 1700);//Returns to original position
 	vroomPush(-127, 127, 400);//Turns towards set of three stars to get any that the claw-bot missed
 	vroomPush(127, 127, 2000);//forward to grab ^ stars
-	vroomPush(-127, 127, 250);//turns towards fence on other side of field
+	vroomPush(-127, 127, 400);//turns towards fence on other side of field
 	vroomPush(127, 127, 1700);//Forward to shove stars under fence
-	for(int q = 0; q < 4; q++){
+	for(int q = 0; q < 4; q++){//Shoves star(s) under fence
 		vroomPush(-127, -127, 500);
 		vroomPush(127, 127, 500);
 	}
-	vroomPush(-127, -127, 1000);
+	vroomPush(-127, -127, 1000);//Back to far zone for driver control
 }
